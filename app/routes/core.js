bce8ca1e757cfaffeebffdd78ae6c2de4ad803d6
root <root@3>
2018-11-20 16:43:47 -0200

    some optimizations

--- app/routes/core.js
+++ app/routes/core.js
@@ -1,10 +1,13 @@
 'use strict';
+var express = require('express');
 
 module.exports = function(app) {
   var core = require('../controllers/core.js');
 //    app.get(/\.(.+)$/, core.resource );
   app.get('/', core.vueresource );
   app.get(/static\/.*/, core.vueresource );
+  // app.get('/', express.static('../dist') );
+  // app.get(/static\/.*/, express.static('../dist') );
   app.get('/media/:folder/:file', core.resource );
   app.get('/:shorten', core.redirect );
   app.get(/(.*)/, core.vueresource );
